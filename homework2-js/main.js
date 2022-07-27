2d3e2db13399c8ba9974dfe86073e9d656461fbf
petrivnm@gmail.com <petrivnm@gmail.com>
2022-07-27 14:28:33 +0200

    Initial commit

---
+++ homework2-js/main.js
@@ -0,0 +1,80 @@
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+//     let clients = [
+//         {name:'Ira',surname:'Petriv',city:'Belz',age:31},
+//         {name:'Ihor',surname:'Makachka',city:'Kiev',level:26},
+//         {name:'Galyna',surname:'Vorona',city:'Sokal',level:32},
+//         {name:'Olia',surname:'Vorona',city:'Lviv',level:28},
+//         {name:'Lyda',surname:'Tsaryk',city:'Vynnyky',level:24},
+//         {name:'Orysya',surname:'Kokot',city:'Radehiv',level:21},
+//         {name:'Sofia',surname:'Chernovol',city:'Lviv',level:29},
+//         {name:'Andrij',surname:'Sereda',city:'Sokal',level:25},
+//         {name:'Yaroslav',surname:'Shelemej',city:'Chervonograd',level:22},
+//         {name:'Yra',surname:'Shahoval',city:'Hmelnytsk',level:20}
+//     ]
+// console.log(clients[0]);
+// console.log(clients);
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+// let objBook1 = {
+//     title: 'Atlant rozpravyv plechi 1',
+//     pageCount: 600,
+//     genre: 'novel'
+// }
+// let objBook2 = {
+//     title: 'Atlant rozpravyv plechi 2',
+//     pageCount: 650,
+//     genre: 'novel'
+// }
+// let objBook3 = {
+//     title: 'Atlant rozpravyv plechi 3',
+//     pageCount: 700,
+//     genre: 'novel'
+// }
+// console.log(objBook1)
+
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+// let objBook1 = {
+//     title: 'Big money',
+//     pageCount: 300,
+//     genre: 'bussines',
+//     authors: ['Chernyak',51]
+// }
+// let objBook2 = {
+//     title: 'financial history of the world',
+//     pageCount: 680,
+//     genre: 'bussines',
+//     authors: ['Nil Fergjson',59]
+// }
+// let objBook3 = {
+//     title: 'God Father',
+//     pageCount: 500,
+//     genre: 'novel',
+//     authors: ['Mario Puso','rip']
+// }
+
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
+//     let user = [
+//         {name: 'Ira', surname: 'Petriv', password:'qwerty'},
+//         {name: 'Ihor', surname: 'Makachka', password:'donatello!'},
+//         {name: 'Galyna', surname: 'Vorona', password:'papakarlo?'},
+//         {name: 'Olia', surname: 'Vorona', password:'Lviv1586'},
+//         {name: 'Lyda', surname: 'Tsaryk', password:'%%%777Abc!'},
+//         {name: 'Orysya', surname: 'Kokot', password:'123456789'},
+//         {name: 'Sofia', surname: 'Chernovol', password:'1111'},
+//         {name: 'Andrij', surname: 'Sereda', password:'0000'},
+//         {name: 'Yaroslav', surname: 'Shelemej', password:'Obama!'},
+//         {name: 'Yra', surname: 'Shahoval', password:'whitList'}
+//     ]
+// console.log(user[0].name,user[0].password);
+// console.log(user[1].name,user[1].password);
+// console.log(user[2].name,user[2].password);
+// console.log(user[3].name,user[3].password);
+// console.log(user[4].name,user[4].password);
+// console.log(user[5].name,user[5].password);
+// console.log(user[6].name,user[6].password);
+// console.log(user[7].name,user[7].password);
+// console.log(user[8].name,user[8].password);
+// console.log(user[9].name,user[9].password);
